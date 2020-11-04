d2558d4e4c904fd01b8c6ec00639dd4150103dd5
OIOIOOOI <anatolii.mn@gmail.com>
2020-11-04 13:11:20 +0200

    EASYOPAC-1254 - Move what's important from culture.

--- modules/ding_event/templates/views-exposed-form-widget.tpl.php
+++ modules/ding_event/templates/views-exposed-form-widget.tpl.php
@@ -9,30 +9,14 @@ $element_name = $element['#name'] . '[]';
 $element_id = $element['#id'];
 ?>
 
-<div class="views-exposed-form-widget accordion-wrapper" id="<?php print $element_id; ?>">
-  <?php if (isset($title)): ?>
-    <div class="accordion-filter-header">
-      <a class="accordion-control" data-toggle="collapse" data-target="#<?php print $element_id; ?>-conteiner" aria-controls="<?php print $element_id; ?>-conteiner">
-        <?php print $title; ?>
-        <span class="arrow"></span>
-      </a>
-    </div>
-  <?php else: ?>
-    <div class="accordion-filter-header">
-      <a class="accordion-control" data-toggle="collapse" data-target="#<?php print $element_id; ?>-conteiner" aria-controls="<?php print $element_id; ?>-conteiner">
-        <?php print $element['#title']; ?>
-        <span class="arrow"></span>
-      </a>
-    </div>  
-  <?php endif; ?>
-  <div id="<?php print $element_id; ?>-conteiner" class="collapse accordion-filter-body">
-    <ul class="sub-menu">
-      <?php foreach ($items as $key => $item): ?>
-        <li>
-          <input type="checkbox" id="<?php print $element_id . '-' . $key; ?>" name="<?php print $element_name; ?>" value="<?php print $key; ?>">
-          <label for="<?php print $element_id . '-' . $key; ?>" class="menu-item"><?php print $item; ?></label>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
+<div class="views-exposed-form-widget" id="<?php print $element_id; ?>">
+  <h2 class="sub-menu-title"><?php print $title; ?></h2>
+  <ul class="sub-menu">
+    <?php foreach ($items as $key => $item): ?>
+      <li>
+        <input type="checkbox" id="<?php print $element_id . '-' . $key; ?>" name="<?php print $element_name; ?>" value="<?php print $key; ?>">
+        <label for="<?php print $element_id . '-' . $key; ?>" class="menu-item"><?php print $item; ?></label>
+      </li>
+    <?php endforeach; ?>
+  </ul>
 </div>
